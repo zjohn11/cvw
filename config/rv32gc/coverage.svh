deefe62205171d5d97c7eec64958e86a58235fdd
zjohn11 <122994816+zjohn11@users.noreply.github.com>
2024-10-16 12:36:46 -0500

    Adding for merge

---
+++ config/rv32gc/coverage.svh
@@ -0,0 +1,16 @@
+// coverage.svh
+// David_Harris@hmc.edu 7 September 2024
+// SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+
+// This file is needed in the config subdirectory for each config supporting coverage.
+// It defines which extensions are enabled for that config.
+
+`include "RV32I_coverage.svh"
+`include "RV32M_coverage.svh"
+`include "RV32F_coverage.svh"
+`include "RV32Zfh_coverage.svh"
+`include "RV32Zicond_coverage.svh"
+`include "RV32Zca_coverage.svh"
+`include "RV32Zcb_coverage.svh"
+`include "RV32ZcbM_coverage.svh"
+`include "RV32ZcbZbb_coverage.svh"
