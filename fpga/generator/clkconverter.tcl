a9e37d0b4fef547189ac51fd905e63932ee35e5b
zjohn11 <122994816+zjohn11@users.noreply.github.com>
2024-10-16 12:57:49 -0500

    Update and rename xlnx_axi_clock_converter.tcl to clkconverter.tcl

--- fpga/generator/clkconverter.tcl
+++ fpga/generator/clkconverter.tcl
@@ -4,7 +4,7 @@ set boardName $::env(XILINX_BOARD)
 #set partNumber xcvu9p-flga2104-2L-e
 #set boardName  xilinx.com:vcu118:part0:2.4
 
-set ipName xlnx_axi_clock_converter
+set ipName clkconverter
 
 create_project $ipName . -force -part $partNumber
 if {$boardName!="ArtyA7"} {
